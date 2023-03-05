da08a65192a23619ea75b60e58d93979f907886a
Jerry Ho <jerryho@Jerrys-MacBook-Air.local>
2023-03-05 14:43:43 +0800

    initial commit

---
+++ public/utils/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDjmsTH99xmRmiWv9urG2ZBaQ52tJbPjBc",
+  authDomain: "get-resume.firebaseapp.com",
+  databaseURL: "https://get-resume.firebaseio.com",
+  projectId: "get-resume",
+  storageBucket: "get-resume.appspot.com",
+  messagingSenderId: "14659346339",
+  appId: "1:14659346339:web:01150f2e0aa1acf9434416"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
